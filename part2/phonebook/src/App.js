6295f48f697c2dab7c0305444dd581d3e25782bf
alvinle29 <bach938@gmail.com>
2022-09-13 11:15:23 +0300

    done 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,51 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons
+      .map(person => person.name)
+      .includes(newName)
+    ) {
+      return window.alert(
+        `${newName} is already added to phonebook`
+      )
+    }
+
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNameChange} 
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+      <p key={person.name}>
+        {person.name}
+      </p>)}
     </div>
   )
 }
