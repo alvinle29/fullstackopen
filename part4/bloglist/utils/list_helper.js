ee8da3e9a81a706eefdfd002507dd393c70bd16b
alvinle29 <bach938@gmail.com>
2022-09-29 16:38:16 +0300

    done 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -41,9 +41,34 @@ const mostBlogs = (blogs) => {
   return result
 }
 
+const mostLikes = (blogs) => {
+  const authorLikes = {}
+  const result = {
+    author: '',
+    likes: 0
+  }
+
+  blogs.forEach((blog) => {
+    authorLikes[blog.author] = authorLikes[blog.author]
+      ? authorLikes[blog.author] + blog.likes
+      : blog.likes
+  })
+
+  for (const [key, value] of Object.entries(authorLikes)) {
+    if (value > result.likes) {
+      result.author = key
+      result.likes = value
+    }
+  }
+   console.log(authorLikes)
+
+  return result
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
