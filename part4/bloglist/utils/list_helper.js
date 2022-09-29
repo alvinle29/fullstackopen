a0d3ab2bc62ebe349ac8670681c596b2290fa35b
alvinle29 <bach938@gmail.com>
2022-09-29 14:55:37 +0300

    done 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+
+  const result = blogs.filter(blog => blog.likes === mostLikes)[0]
+
+  return {
+    'title': result.title,
+    'author': result.author,
+    'likes': result.likes,
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
