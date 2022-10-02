87a457318124ef8070608d406911eaa3a99caaa5
alvinle29 <bach938@gmail.com>
2022-10-02 23:48:40 +0300

    done 4.16

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,14 @@
 const config = require('./utils/config')
 const express = require('express')
+
 const app = express()
+
+const mongoose = require('mongoose')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 const blogRouter = require("./controllers/blog")
+const userRouter = require('./controllers/user')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
