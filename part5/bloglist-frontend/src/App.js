bf097899be6831e9b197ad79cf3dc4e404853171
alvinle29 <bach938@gmail.com>
2022-11-08 22:32:52 +0200

    done 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -138,13 +138,14 @@ const App = () => {
   const blogForm = () => (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          handleLike={() => handleLike(blog)}
-        />
-      )}
+      {blogs.sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={() => handleLike(blog)}
+          />
+        )}
     </div>
   )
 
