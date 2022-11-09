83a036bdf854713c8558d9fa21574727758224b3
alvinle29 <bach938@gmail.com>
2022-11-09 14:00:34 +0200

    done 5.14

--- part5/bloglist-frontend/src/test/Blog.test.js
+++ part5/bloglist-frontend/src/test/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 test('blog renders the blog\'s title and author, but does not render its url or number of likes by default', () => {
@@ -37,6 +37,38 @@ test('blog renders the blog\'s title and author, but does not render its url or 
     'Stewart Dunky'
   )
   expect(component.container).not.toHaveTextContent(
+    5
+  )
+})
+
+test('Test which checks that the blog\'s url and number of likes are shown when the button controlling the shown details has been clicked.', () => {
+  const blog = {
+    title: 'Blog title',
+    author: 'Blog Author',
+    url: 'abcxyz.com',
+    user: 'Stewart Dunky',
+    likes: 5
+  }
+  const user = 'Stewart Dunky'
+  const handleDelete = () => null
+  const handleLike = () => null
+
+  const component = render(
+    <Blog
+      blog={blog}
+      user={user}
+      handleDelete={handleDelete}
+      handleLike={handleLike}
+    />
+  )
+
+  const viewButton = component.getByText('view')
+  fireEvent.click(viewButton)
+
+  expect(component.container).toHaveTextContent(
+    'abcxyz.com'
+  )
+  expect(component.container).toHaveTextContent(
     5
   )
 })
