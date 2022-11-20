449bcf1f8511722931078d1f65a6455677711f31
alvinle29 <bach938@gmail.com>
2022-11-20 17:54:43 +0200

    done 5.18

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -4,11 +4,34 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
+  const user = {
+    name: 'Alvin',
+    username: 'alvinle29',
+    password: 'abcd1234'
+  }
+  cy.request('POST', 'http://localhost:3003/api/users/', user)
+
   it('Login form is shown', function () {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
   })
 
-  
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('alvinle29')
+      cy.get('#password').type('abcd1234')
+      cy.get('#login-button').click()
+
+      cy.contains('Alvin logged-in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('alvinle')
+      cy.get('#password').type('1231234')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong credentials', { matchCase: false })
+    })
+  })
 })
