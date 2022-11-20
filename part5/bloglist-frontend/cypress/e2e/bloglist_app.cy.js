0c9fc2cc99073bdf99f2ab954562fd022826f097
alvinle29 <bach938@gmail.com>
2022-11-20 17:52:54 +0200

    done 5.17

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,22 +1,14 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    const user = {
-      name: 'Gustav Nachtigal',
-      username: 'gustav',
-      password: '123qwe'
-    }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
-    cy.contains('log in to application')
-    cy.contains('Login')
-    cy.get('#username').type('gustav')
-    cy.get('#password').type('123qwe')
-    cy.get('#login-button').click()
-
-    cy.contains('Gustav Nachtigal logged in')
+  it('Login form is shown', function () {
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
   })
+
+  
 })
